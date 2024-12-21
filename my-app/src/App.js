f32192051c1183c700f98f3f3d99a94e8df3ae02
viraj200524 <virajv2005@gmail.com>
2024-12-21 22:29:00 +0530

    successfully-merged

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,28 @@
-import {Routes,Route} from 'react-router-dom';
+import './App.css';
+
+import Navbar from './components/NavBar.js';
+import TextEditor from './components/TEXTEDITOR/TextEditor.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 import HomePage from './pages/Home';
 import RoomPage from './pages/Room';
 function App() {
   return (
-  <Routes>
-    <Route path='/' element={<HomePage />} />
-    <Route path='/room/:roomId' element={<RoomPage/>}/>
-  </Routes>
+    <Router>
+      <div>
+        <Routes>
+          {/* Route for the document with a dynamic ID */}
+          <Route path="/home" element={<Navbar />} />
+          <Route path='/' element={<HomePage />} />
+          <Route path='/room/:roomId' element={<RoomPage/>}/>
+          <Route path="/document/:id" element={<TextEditor />} />
+        </Routes>
+      </div>
+    </Router>
+ 
+    
+  
   );
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './style.css';
+
+import App from './App.js';
+import reportWebVitals from './reportWebVitals.js';
+
 
 import { BrowserRouter } from 'react-router-dom';
 
